bd5406e27cb395f128022a4dceb93f8d3b960893
faizahsayyid <faizah.sayyid@gmail.com>
2022-01-16 10:34:40 -0500

    connecting backend to frontend

--- client/src/contexts/GlobalContext.js
+++ client/src/contexts/GlobalContext.js
@@ -2,7 +2,16 @@ import GLOBAL_ACTIONS from "./GlobalActions";
 import { createContext, useReducer } from "react";
 import GlobalReducer from "./GlobalReducer";
 
-const initialState = { token: "", isLoggedIn: false, username: "" };
+const initialState = {
+  token: "",
+  isLoggedIn: false,
+  username: "",
+  sentiment: null,
+  topicsCovered: null,
+  isAnalysisLoading: false,
+  question: "",
+  audioURL: "",
+};
 
 export const GlobalContext = createContext(initialState);
 
@@ -20,15 +29,54 @@ export const GlobalProvider = ({ children }) => {
     dispatch({ type: GLOBAL_ACTIONS.SET_IS_LOGGED_IN, payload: username });
   };
 
+  const setSentiment = (sentiment) => {
+    dispatch({ type: GLOBAL_ACTIONS.SET_SENTIMENT, payload: sentiment });
+  };
+
+  const setTopics = (topics) => {
+    dispatch({ type: GLOBAL_ACTIONS.SET_TOPICS, payload: topics });
+  };
+
+  const setIsAnalysisLoading = (isAnalysisLoading) => {
+    dispatch({
+      type: GLOBAL_ACTIONS.SET_IS_ANALYSIS_LOADING,
+      payload: isAnalysisLoading,
+    });
+  };
+
+  const setAudioUrl = (audioUrl) => {
+    dispatch({
+      type: GLOBAL_ACTIONS.SET_AUDIO_URL,
+      payload: audioUrl,
+    });
+  };
+
+  const setQuestion = (question) => {
+    dispatch({
+      type: GLOBAL_ACTIONS.SET_QUESTION,
+      payload: question,
+    });
+  };
+
   return (
     <GlobalContext.Provider
       value={{
         token: state.token,
         isLoggedIn: state.isLoggedIn,
         username: state.username,
+        sentiment: state.sentiment,
+        topicsCovered: state.topicsCovered,
+        isAnalysisLoading: state.isAnalysisLoading,
+        question: state.question,
+        audioURL: state.audioURL,
         setToken,
         setIsLoggedIn,
         setUsername,
+        setSentiment,
+        setTopics,
+        setIsAnalysisLoading,
+        setAudioUrl,
+        setQuestion,
       }}
     >
       {children}
